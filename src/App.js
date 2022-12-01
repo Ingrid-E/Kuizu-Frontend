b63a27119cfddf8812e2df521edb864aa393f395
Ingrid Echeverri <ingrid.echeverri@correounivalle.edu.co>
2022-11-30 20:49:00 -0500

    Testing google login configuration

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import logo from './logo.svg';
 import './App.css';
+import GoogleLogin from './components/google-login/google-login';
 
 function App() {
   return (
     <div className="App">
       <h1>Chismoso</h1>
+      <GoogleLogin></GoogleLogin>
     </div>
   );
 }
