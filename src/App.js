f259dc018cf8caef01655c8e48c3f9f21e4a3b40
Ingrid Echeverri <ingrid.echeverri@correounivalle.edu.co>
2022-12-03 10:04:53 -0500

    Google login component added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React from 'react'
-import logo from './logo.svg';
 import './App.css';
-import GoogleLogin from './components/google-login/google-login';
+import LoginGoogle from './components/google-login/google-login';
 
 function App() {
   return (
     <div className="App">
       <h1>Chismoso</h1>
-      <GoogleLogin></GoogleLogin>
+      <LoginGoogle></LoginGoogle>
     </div>
   );
 }
