901209ff530ad2042f3d08ef62c4673a0107a58e
Jean Pierre Cardenas Perea <jpcardenas190@hotmail.com>
2023-01-04 12:59:49 -0500

    agregando diseño de la vista principal del dashboard

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+export { default as CircleImage } from "./circle-image/CircleImage";
+export { default as AverageCard } from "./average-card/AverageCard";
+export { default as TestCard } from "./test-card/TestCard";
+export { default as CourseCard } from "./course-card/CourseCard";
+export { default as LineRule } from "./line-rule/LineRule";
---
+++ src/layouts/index.js
@@ -0,0 +1,6 @@
+export { default as Navbar } from "./navbar/Navbar";
+export { default as DashboardHome } from "./dashboard-home/DashboardHome";
+export { default as DashboardHeading } from "./dashboard-heading/DashboardHeading";
+export { default as DashboardAverages } from "./dashboard-averages/DashboardAverages";
+export { default as Activities } from "./activities/Activities";
+export { default as Courses } from "./courses/Courses";
