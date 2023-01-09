dc0b43965f7c7fae366a287596fac79e5d91883f
Ingrid Echeverri <ingrid.echeverri@correounivalle.edu.co>
2023-01-09 10:51:09 -0500

    File name changed to M

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,3 @@
 export {default as Home} from './home/Home'
-export {default as Dashboard} from './dashboard/dashboard'
+export {default as Dashboard} from './dashboard/Dashboard'
 export {default as InProgress} from './in-progress/InProgress'
